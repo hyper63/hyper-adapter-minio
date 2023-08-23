adcf222c1361a2939cb8ca08573593bf3c81e087
Tyler Hall <tyler@hyper.io>
2023-08-23 15:58:20 +0000

    feat: use zod schema to parse inputs into minio client

--- deps.js
+++ deps.js
@@ -2,7 +2,6 @@ export * as R from 'https://cdn.skypack.dev/ramda@0.29.0'
 export { default as crocks } from 'https://cdn.skypack.dev/crocks@0.12.4'
 
 export * as Minio from 'npm:minio@7.1.1'
-
 /**
  * readable-stream https://www.npmjs.com/package/readable-stream
  * which is a mirror of Node's streams impls, which now has conversion apis ie. "fromWeb"
@@ -13,6 +12,7 @@ export * as Minio from 'npm:minio@7.1.1'
 export { ReadableWebToNodeStream } from 'npm:readable-web-to-node-stream@3.0.2'
 
 export { join } from 'https://deno.land/std@0.199.0/path/mod.ts'
+export { z } from 'https://deno.land/x/zod@v3.20.5/mod.ts'
 
 export {
   HyperErr,
